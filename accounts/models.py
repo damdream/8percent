6b61bfa757154b3abc15774d4e21df0220e22be9
jinatra <jinatra0816@gmail.com>
2021-11-11 22:00:52 +0900

    앱 추가 / 모델링 적용

---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.db                       import models
+
+from users.models import User
+from core.models  import TimeStampModel
+
+class Account(models.Model):
+    balance = models.DecimalField(max_digits=16, decimal_places=2)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'accounts'
+
+class Transaction(TimeStampModel):
+    balance      = models.DecimalField(max_digits=16, decimal_places=2)
+    type         = models.PositiveIntegerField()
+    brief        = models.TextField(max_length=100)
+    user         = models.ForeignKey(User, on_delete=models.Case)
+
+    class Meta:
+        db_table = 'transactions'
--- percent/settings.py
+++ percent/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'users',
+    'accounts',
+    'core',
 ]
 
 MIDDLEWARE = [
