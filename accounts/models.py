d1b27d7605b2c090887785dbb083a884943320ce
jinatra <jinatra0816@gmail.com>
2021-11-11 23:15:51 +0900

    모델링 변경 사항 반영

--- accounts/models.py
+++ accounts/models.py
@@ -5,16 +5,17 @@ from core.models  import TimeStampModel
 
 class Account(models.Model):
     balance = models.DecimalField(max_digits=16, decimal_places=2)
-    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
 
     class Meta:
         db_table = 'accounts'
 
 class Transaction(TimeStampModel):
-    balance      = models.DecimalField(max_digits=16, decimal_places=2)
-    type         = models.PositiveIntegerField()
-    brief        = models.TextField(max_length=100)
-    user         = models.ForeignKey(User, on_delete=models.Case)
+    balance           = models.DecimalField(max_digits=16, decimal_places=2)
+    transaction_money = models.DecimalField(max_digits=16, decimal_places=2)
+    type              = models.PositiveIntegerField()
+    brief             = models.TextField(max_length=100)
+    user              = models.ForeignKey(User, on_delete=models.Case)
 
     class Meta:
         db_table = 'transactions'
