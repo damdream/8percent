111bdcdbb965a3be3965595139da73d078532cc9
jinatra <jinatra0816@gmail.com>
2021-11-11 23:16:59 +0900

    모델링 변경 사항 반영

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,7 @@ class Transaction(TimeStampModel):
     transaction_money = models.DecimalField(max_digits=16, decimal_places=2)
     type              = models.PositiveIntegerField()
     brief             = models.TextField(max_length=100)
-    user              = models.ForeignKey(User, on_delete=models.Case)
+    user              = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'transactions'
